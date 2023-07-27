c506c64df1261924b961c2182b6bbf3129e90873
Batuhan Göktaş <mbg.is.king@gmail.com>
2023-07-27 19:53:42 +0300

    moved welcome view into a seperate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learner_app/firebase_options.dart';
+import 'package:learner_app/views/auth/welcome.dart';
 import 'package:learner_app/views/home/add_course.dart';
 import 'package:learner_app/views/auth/sign_in.dart';
 import 'package:learner_app/views/auth/sign_up.dart';
@@ -33,82 +34,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Welcome extends StatelessWidget {
-  const Welcome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 120.0,
-          ),
-          const Text(
-            "Welcome to",
-            style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.w700,
-                color: Color.fromARGB(200, 0, 0, 0)),
-          ),
-          const Text(
-            "Learner",
-            style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.w700,
-                color: Color.fromARGB(200, 0, 0, 0)),
-          ),
-          const SizedBox(
-            height: 100.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black87,
-                  fixedSize: const Size(150.0, 75.0),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    "/signup/",
-                    (route) => false,
-                  );
-                },
-                child: const Text(
-                  "Sign Up",
-                  style: TextStyle(
-                      fontSize: 31.24,
-                      color: Color.fromARGB(223, 255, 255, 255)),
-                ),
-              ),
-              const SizedBox(
-                width: 20.0,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black87,
-                  fixedSize: const Size(150.0, 75.0),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    "/signin/",
-                    (route) => false,
-                  );
-                },
-                child: const Text(
-                  "Sign In",
-                  style: TextStyle(
-                      fontSize: 31.24,
-                      color: Color.fromARGB(223, 255, 255, 255)),
-                ),
-              ),
-            ],
-          ),
-        ],
-      )),
-    );
-  }
-}
---
+++ lib/views/auth/welcome.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 120.0,
+          ),
+          const Text(
+            "Welcome to",
+            style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.w700,
+                color: Color.fromARGB(200, 0, 0, 0)),
+          ),
+          const Text(
+            "Learner",
+            style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.w700,
+                color: Color.fromARGB(200, 0, 0, 0)),
+          ),
+          const SizedBox(
+            height: 100.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black87,
+                  fixedSize: const Size(150.0, 75.0),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    "/signup/",
+                    (route) => false,
+                  );
+                },
+                child: const Text(
+                  "Sign Up",
+                  style: TextStyle(
+                      fontSize: 31.24,
+                      color: Color.fromARGB(223, 255, 255, 255)),
+                ),
+              ),
+              const SizedBox(
+                width: 20.0,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black87,
+                  fixedSize: const Size(150.0, 75.0),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    "/signin/",
+                    (route) => false,
+                  );
+                },
+                child: const Text(
+                  "Sign In",
+                  style: TextStyle(
+                      fontSize: 31.24,
+                      color: Color.fromARGB(223, 255, 255, 255)),
+                ),
+              ),
+            ],
+          ),
+        ],
+      )),
+    );
+  }
+}
